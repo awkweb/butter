2d69407f0d0115f8a34a460c75a8d2b0cd5ed8bc
Tom Meagher <thom.meagher@gmail.com>
2018-09-17 23:21:28 -0400

    Set up prettier and vue cli

--- api/server/api/models/__init__.py
+++ api/server/api/models/__init__.py
@@ -1,6 +1,1 @@
 from .user import User
-from .balance import Balance
-from .institution import Institution
-from .transaction_location import TransactionLocation
-from .account import Account
-from .transaction import Transaction
