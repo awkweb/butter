d4140df05e5d517d31d9a0735ef191ca86b75ada
Tom Meagher <thom.meagher@gmail.com>
2019-02-27 22:12:59 -0500

    Remove aws-compose.yml

--- scripts/setup-aws-ecs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# configure
-echo "Configuring cluster"
-ecs-cli configure --region us-east-1 --cluster vodka-soda
-
-# setup cloud formation template
-echo "Creating cluster"
-ecs-cli up --keypair vodka-soda --capability-iam --size 2 --vpc vpc-80bfc5fb --subnets subnet-1b96637c,subnet-18756b45 --instance-type t2.micro
